17edc857a2491236fed6acd75b2e3a6e44351413
Mike Patterson <mpatterson73@gmail.com>
2020-01-06 09:09:38 -0500

    create dead_state and random_state boards

--- life.py
+++ life.py
@@ -5,12 +5,26 @@
 
 import numpy as np
 
+def get_state
+
 def dead_state(width=5, height=5):
     # Create a board of declared width and height
     # with all cells initialized to dead.
-    board = np.zeros((height,width),dtype=int)
-    return board
+    state_grid = np.zeros((height,width),dtype=int)
+    return state_grid
+
+def random_state(width=10, height=5):
+    #board = dead_state(width, height)
+    s = (height, width)
+    state_grid = np.random.sample(size=s)
+    state_grid[]
+    return state_grid
+    
+def render(state):
+    # take a grid of cell states & render a board
+
+    return
 
 
-print(dead_state(10,5))
+print(random_state(10,5))
 
